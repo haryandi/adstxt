65ed3a335df0cd373704a72e26cd0ae5ab2ee694
Haryandi Andi H'Seven <38398405+haryandi@users.noreply.github.com>
2024-01-19 21:51:12 +0700

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -48,7 +48,7 @@ const request = (method, path) => {
 
     return {
         async then(...params) {
-            return fetch(url + path, req)
+            return fetch(url + path, req,{mode: 'cors'})
                 .then((res) => res.json())
                 .then((res) => {
                     if (res.error !== null) {
